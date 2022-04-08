4a0134975ebce8e79983ce403756db0048967bda
accuaccu <paavo.raittinen@gmail.com>
2022-04-08 19:54:49 +0300

    Initial commit

---
+++ 90_presets/graphic_presets.R
@@ -0,0 +1,5 @@
+pcol <- c("#FFFFFFD9", '#1f2828', '#e85836', '#6c8d8d', '#ccb100')
+op.web <- par(family = 'serif', bg = 'white',
+              cex.axis = 1.25, cex.lab = 1.5, cex.main = 1.5,
+              col.axis = pcol[2], col.lab = pcol[2], col.main = pcol[2], col.sub = pcol[2],
+              font.lab = 3, font.axis = 3, fg = pcol[2], tck = 0.015)
