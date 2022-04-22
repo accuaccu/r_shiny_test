6110b8d5d1b735158dbde1916c7e3d5be00ff241
accuaccu <paavo.raittinen@gmail.com>
2022-04-22 13:50:14 +0300

    Added some explanation and demonstration on wilcox.test functionality and misundestanding about how the test statistic is calculated

--- 01_programs/simulate_and_demo_rank_sum.R
+++ 01_programs/simulate_and_demo_rank_sum.R
@@ -48,3 +48,47 @@ plot(y = 1:nrow(z_size_m), x = rep(1.5,40), cex = z_size_m[order(D),D],  pch = 1
 
 # Make wilcox.test
 wilcox.test(z_size_m$D ~ z_size_m$TEAM)
+
+#
+
+
+
+fun.w.simu <- function(x,y) {
+  n1 <- length(x)
+  n2 <- length(y)
+  
+  z <- c(x,y)
+  
+  Z <- data.table(XY = z, S = c(rep('X',n1),rep('Y',n2)))
+  Z[, Z_RANK := rank(XY)]
+  
+  
+  R1 <- Z[S == 'X', sum(Z_RANK)]
+  R2 <- Z[S == 'Y', sum(Z_RANK)]
+  
+  
+  U1 <- R1 - (n1*(n1+1) / 2)
+  U2 <- R2 - (n2*(n2+1) / 2)
+  
+  p1 <- pwilcox(U1 - 1, n1, n2, lower.tail = FALSE)
+  p2 <- pwilcox(U2 - 1, n1, n2, lower.tail = FALSE)
+  
+  p1 <- min(2 * p1, 1)
+  p2 <- min(2 * p2, 1)
+  
+  print(paste0('The p-value for test statistic U1 = ', U1, ' is ', p1))
+  print(paste0('The p-value for test statistic U2 = ', U2, ' is ', p2))
+  
+  wilcox.test(Z$XY ~ Z$S)
+}
+
+
+fun.w.simu(x = rnorm(12), y = rnorm(13))
+
+# hmm...it is almost true, i.e., the Mann-Whitney U test test statistic U and Wilcoxon rank sum test test statistic W
+# are almost the same, but it is in the MW test that the U is selectect as the min(U1, U2), but it is not
+# so in the wilcox test; in the R wilcox.test implementation it is actually THE largest, i.e., max(U1, U2)
+# that is used!
+
+# However, this is noted in the wilcox.test documentation, altough, somewhat 
+# implicitly.
